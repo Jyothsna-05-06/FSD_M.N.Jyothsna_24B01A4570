75f860c3362a5e07bddd93441192543d9abe2abd
Jyothsna-05-06 <mjyothsna18@gmail.com>
2026-07-18 09:38:13 +0530

    Add files via upload

---
+++ datatypes.ts
@@ -0,0 +1,70 @@
+//String
+console.log("-----String------");
+let myname:string = "Jyothsna";
+console.log("My name is " + myname);
+
+let str:string = "Hello, World";
+console.log(str);
+
+let fruit:string = "Mango";
+console.log("My favourite fruit is " + fruit);
+
+//Number
+console.log("-----Number-----");
+let num:number = 10;
+console.log(num);
+
+let age:number = 20;
+console.log("My age is " +age);
+
+let a:number = 12;
+let b:number = 2;
+let sum:number = a + b;
+console.log("The sum of a and b: " + sum);
+
+//boolean
+console.log("-----Boolean-----");
+let found:boolean = true;
+console.log(found);
+
+let notfound:boolean = false;
+console.log(notfound);
+
+let f1:boolean = true;
+let f2:boolean = false;
+console.log(f1 && f2);
+console.log(f1 || f2);
+
+//null
+console.log("-----Null-----");
+let ab:null = null;
+console.log(ab);
+
+let city:null = null;
+console.log(city);
+
+let f:null = null;
+console.log("f is:" +f);
+
+//undefined
+console.log("-----Undefined-----");
+let car:undefined = undefined;
+console.log(car);
+
+let game:undefined = undefined;
+console.log(game);
+
+let village:undefined = undefined;
+console.log("The Village is:" +village);
+
+//bigint
+console.log("-----Bigint-----");
+let big:bigint = 1234634363536353646n;
+console.log(big);
+
+let bigNum:bigint = 88898987978n;
+console.log(bigNum + "is very big number");
+
+let a1:bigint = 100n;
+let a2:bigint = 150n;
+console.log(a1 + a2);
